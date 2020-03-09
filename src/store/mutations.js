35883ea1cb28a42c4bf51f9fcdaf12c8dabc7693
mengyaqi <2686587087@qq.com>
2020-03-09 22:43:20 +0800

    second

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,6 +5,7 @@ export default {
       payload.count += 1;
     },
     ADD_TO_CART(state,payload){
+      payload.check = false
       state.cartList.push(payload)
     }
   }
--- src/views/cart/childComps/CartListItem.vue
+++ src/views/cart/childComps/CartListItem.vue
@@ -1,12 +1,30 @@
 <template>
-    <div class="cart-list-item">
-        {{product}}
+    <div id="shop-item">
+       <div class="item-selector">
+           <checkButton :isCheck="product.check" @click.native='checkClick'/>
+       </div>
+       <div class="item-img">
+           <img :src="product.image" alt="商品图片">
+       </div>
+       <div class="item-info">
+           <div class="item-title">{{product.title}}</div>
+           <div class="item-desc">{{product.desc}}</div>
+           <div class="item-bottom">
+               <div class="item-price left">￥{{product.price}}</div>
+               <div class="item-count right">×{{product.count}}</div>
+           </div>
+       </div>
     </div>
 </template>
 
 <script>
+    import CheckButton from 'components/content/checkButton/CheckButton'
+
 export default {
     name:'CartListItem',
+    components:{
+        CheckButton
+    },
     props:{
         product:{
             type:Object,
@@ -14,10 +32,67 @@ export default {
                 return {}
             }
         }
+    },
+    methods:{
+        checkClick(){
+            this.product.check = !this.product.check
+        }
     }
 }
 </script>
 
 <style scoped>
+    #shop-item {
+        width: 100%;
+        display: flex;
+        font-size: 0;
+        padding: 5px;
+        border-bottom: 1px solid #ccc;
+    }
+    .item-selector {
+        width:20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .item-img {
+        padding: 5px;
+    }
+    .item-img img {
+        width: 80px;
+        height: 100px;
+        display: block;
+        border-radius: 5px;
+    }
+
+    .item-info {
+        font-size: 17px;
+        clear: #333;
+        padding: 5px 10px;
+        position: relative;
+        overflow: hidden;
+    }
+    .item-title, .item-desc {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+    .item-info, .item-desc {
+        font-size: 14px;
+        color: #666;
+        margin-top: 15px;
+    }
+    .item-bottom {
+        margin-top: 10px;
+        position: absolute;
+        bottom: 10px;
+        left: 10px;
+        right: 10px;
+    }
+    .intem-bottom,.item-price {
+        color:orangered;
+    }
+
 
 </style>
